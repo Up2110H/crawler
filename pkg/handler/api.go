f635ab5d69f0a748c93cf39f72c7f226b66b1836
up2110h <qudratullayev97@gmail.com>
2022-03-03 23:07:36 +0500

    getTitlesByUrls optimized with goroutines

--- pkg/handler/api.go
+++ pkg/handler/api.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/net/html"
 	"net/http"
+	"sync"
 )
 
 func (h *Handler) getTitlesByUrls(c *gin.Context) {
@@ -16,34 +17,53 @@ func (h *Handler) getTitlesByUrls(c *gin.Context) {
 		return
 	}
 
+	var wg sync.WaitGroup
+	var rspMap = make(map[string]Title)
+	wg.Add(len(input.List))
+
+	work := func(url string) {
+		defer wg.Done()
+		worker(&rspMap, url)
+	}
+
 	for _, url := range input.List {
-		resp, err := http.Get(url)
-		if err != nil {
-			output.Result = append(output.Result, Title{
-				Status:  http.StatusNotFound,
-				Message: err.Error(),
-			})
-			continue
-		}
+		go work(url)
+	}
 
-		body, err := html.Parse(resp.Body)
-		if err != nil {
-			output.Result = append(output.Result, Title{
-				Status:  http.StatusMethodNotAllowed,
-				Message: err.Error(),
-			})
-			continue
-		}
+	wg.Wait()
 
-		title := extractPageTitle(body)
-
-		output.Result = append(output.Result, Title{
-			Status: http.StatusOK,
-			Title:  title,
-		})
+	for _, url := range input.List {
+		output.Result = append(output.Result, rspMap[url])
 	}
 
 	c.JSON(http.StatusOK, output)
+}
+
+func worker(m *map[string]Title, url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		(*m)[url] = Title{
+			Status:  http.StatusNotFound,
+			Message: err.Error(),
+		}
+		return
+	}
+
+	body, err := html.Parse(resp.Body)
+	if err != nil {
+		(*m)[url] = Title{
+			Status:  http.StatusMethodNotAllowed,
+			Message: err.Error(),
+		}
+		return
+	}
+
+	title := extractPageTitle(body)
+
+	(*m)[url] = Title{
+		Status: http.StatusOK,
+		Title:  title,
+	}
 }
 
 func extractPageTitle(node *html.Node) string {
